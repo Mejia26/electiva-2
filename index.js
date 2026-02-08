449275fc67c59f92ef98e080d5f753bc5016065b
Mejia26 <112843498+Mejia26@users.noreply.github.com>
2026-02-07 20:40:05 -0400

    Update console log message with additional details

--- index.js
+++ index.js
@@ -1,1 +1,1 @@
-console.log("Hola Mundo - Esta es una prueba de GitHub Actions para la pracitca #04 de electiva 2");
+console.log("Hola Mundo - Esta es una prueba de GitHub Actions para la pracitca #04 de electiva 2 de parte de Guido Mejia L 2024-0197");
